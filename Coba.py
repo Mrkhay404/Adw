a68f47c5aefb50a427571cb0c0da77595c00a87b
Mrkhay404 <107294719+Mrkhay404@users.noreply.github.com>
2022-06-11 12:49:02 +0700

    Create Coba.py

---
+++ Coba.py
@@ -0,0 +1,23 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+mengetik('Start system...')
+mengetik('.')
+mengetik('.')
+mengetik('Update system...')
+mengetik('••••••••••••••✓')
+mengetik('[+].Starting Crack...')
+mengetik('[+].Aodjgowusjgktud')
+mengetik('Access denied!!')
+mengetik('[+].Restart Crack...')
+mengetik('[+]cc@_##+djfur$$')
+mengetik('Success Cracking..')
+mengetik('[+].Get data System...')
+mengetik('•••••••••••••••✓')
+mengetik ('Waiting login..!')
+mengetik ('[+].done✓')
